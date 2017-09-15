da80e46098dc47d5e9076cadd8cb812c27c7695e
Damilare Joseph <dre4success@live.com>
2017-09-15 13:58:41 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+import reducers from './reducers';
+import Async from './middlewares/async';
+
+const createStoreWithMiddleWare = applyMiddleware(Async)(createStore);
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleWare(reducers)}>
+		<App />
+	</Provider>, document.querySelector('.container'));
 registerServiceWorker();
